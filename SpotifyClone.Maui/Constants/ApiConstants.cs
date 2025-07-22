bfacf67e6a3268b638fe7a891e08232ccecb301d
Hossein Ghelichkhani <60262273+hosseinghDev@users.noreply.github.com>
2025-07-23 01:22:54 +0330

    make http work

--- SpotifyClone.Maui/Constants/ApiConstants.cs
+++ SpotifyClone.Maui/Constants/ApiConstants.cs
@@ -3,6 +3,6 @@
     public static class ApiConstants
     {
         // Use 10.0.2.2 for Android Emulator to connect to localhost
-        public static string BaseUrl = "http://10.0.2.2:7225";
+        public static string BaseUrl = "http://10.0.2.2:5141";
     }
 }
